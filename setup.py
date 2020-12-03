5360d5d2a07d86534fee37cabc754cb3598f0baf
Sajjad Ayobi <43270899+sajjjadayobi@users.noreply.github.com>
2020-12-03 08:02:47 +0330

    add requirements in setup

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import setuptools
 with open("README.md", "r") as fh:
    long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+   
 setuptools.setup(
     name='facelib',
     version='1.5',
@@ -12,6 +16,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sajjjadayobi/FaceLib",
+    install_requires=required,
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
