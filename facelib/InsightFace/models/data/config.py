4be86f7d5e4a5fde77dbe20b8caffc4813211f69
Sajjad Ayobi <43270899+sajjjadayobi@users.noreply.github.com>
2021-01-15 15:47:06 +0330

    new version is here

--- facelib/InsightFace/models/data/config.py
+++ facelib/InsightFace/models/data/config.py
@@ -7,7 +7,7 @@ from torch.nn import CrossEntropyLoss
 
 def get_config(inference=True):
     conf = edict()
-    conf.data_path = Path(os.path.dirname(os.path.realpath(__file__)), 'models/data/')
+    conf.data_path = Path(os.path.dirname(os.path.realpath(__file__)))
     conf.work_path = Path('weights/')
     conf.model_path = conf.work_path / 'models'
     conf.log_path = conf.work_path / 'log'
