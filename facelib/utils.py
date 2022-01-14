7d353eeb780e238d93652b4506df6db0eac66a8e
Sajjad Ayoubi <43270899+sajjjadayobi@users.noreply.github.com>
2022-01-14 19:31:38 +0330

    add desc to tqdm

--- facelib/utils.py
+++ facelib/utils.py
@@ -6,7 +6,9 @@ def download_weight(link, file_name, verbose=True):
     response = requests.get(link, stream=True)
     total_size_in_bytes= int(response.headers.get('content-length', 0))
     block_size = 1024 #1 Kibibyte
-    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, disable=False if verbose else True)
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, 
+                        desc='downloading defualt weights',
+                        disable=False if verbose else True)
     
     with open(file_name, 'wb') as file:
         for data in response.iter_content(block_size):
