d21aaea84d88d6009676f3ad32c31402133c0354
Sajjad Ayoubi <43270899+sajjjadayobi@users.noreply.github.com>
2022-01-14 19:29:18 +0330

    add verbose for download_weight

--- facelib/utils.py
+++ facelib/utils.py
@@ -2,11 +2,11 @@ from tqdm import tqdm
 import requests
 import os
 
-def download_weight(link, file_name):
+def download_weight(link, file_name, verbose=True):
     response = requests.get(link, stream=True)
     total_size_in_bytes= int(response.headers.get('content-length', 0))
     block_size = 1024 #1 Kibibyte
-    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, disable=False if verbose else True)
     
     with open(file_name, 'wb') as file:
         for data in response.iter_content(block_size):
@@ -15,6 +15,4 @@ def download_weight(link, file_name):
     progress_bar.close()
     if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
         exit("ERROR, something went wrong (check your connection)")
-    else:
-        print('the default model weight donwloaded')
         
