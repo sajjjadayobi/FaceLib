823002c52e9812e19ea285ef8aa79a9ef5fa2fad
Sajjad Ayoubi <43270899+sajjjadayobi@users.noreply.github.com>
2022-01-18 11:01:04 +0330

    Update alignment.py

--- facelib/Retinaface/utils/alignment.py
+++ facelib/Retinaface/utils/alignment.py
@@ -11,13 +11,12 @@ from skimage import transform as trans
 
 # reference facial points, a list of coordinates (x,y)
 REFERENCE_FACIAL_POINTS = [
-    [30.29459953,  51.69630051],
-    [65.53179932,  51.50139999],
-    [48.02519989,  71.73660278],
-    [33.54930115,  92.3655014],
-    [62.72990036,  92.20410156]
+    [30.29459953, 51.69630051],
+    [65.53179932, 51.50139999],
+    [48.02519989, 71.73660278],
+    [33.54930115, 87],
+    [62.72990036, 87]
 ]
-
 
 class FaceWarpException(Exception):
     def __str__(self):
